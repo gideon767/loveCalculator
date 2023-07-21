746188a8d0e4e5b53c297537e9a890a54331cc2b
gideon767 <103620692+gideon767@users.noreply.github.com>
2023-07-21 22:03:56 +0000

    Added files to loveCalculator

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+
+document.getElementById("calculateLove").addEventListener("click", ()=>{
+  var yourName = document.getElementById("yourName").value;
+  var theirName = document.getElementById("theirName").value;
+  var random = Math.floor(Math.random()*100 + 1);
+  if (random <= 40) {
+    document.getElementById("result").innerHTML = `Lovescore between ${yourName} and ${theirName} is ${random}%. It won't work.`;
+    document.getElementById("remarks").classList.add("bad");
+    document.getElementById("remarksss").classList.remove("perfect");
+    document.getElementById("remarkss").classList.remove("pairing");
+  } else if (random <=75) {
+    document.getElementById("result").innerHTML = `Lovescore between ${yourName} and ${theirName} is ${random}%. Congratulations, you're pairing.`;
+    document.getElementById("remarkss").classList.add("pairing");
+    document.getElementById("remarksss").classList.remove("perfect");
+    document.getElementById("remarks").classList.remove("bad");
+  } else {
+    document.getElementById("result").innerHTML = `Lovescore between ${yourName} and ${theirName} is ${random}%. Wow! You are made for each other.`;
+    document.getElementById("remarksss").classList.add("perfect");
+    document.getElementById("remarks").classList.remove("bad");
+    document.getElementById("remarkss").classList.remove("pairing");
+  }
+});
