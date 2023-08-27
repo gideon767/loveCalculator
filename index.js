10e105a17af85a35e8d1859bb3525dacc078a2e8
gideon767 <103620692+gideon767@users.noreply.github.com>
2023-08-27 06:49:10 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
 
+var calc = document.getElementById("calculateLove");
+  calc.addEventListener("click", ()=>{
+    var yourName = document.getElementById("yourName").value;
+    var theirName = document.getElementById("theirName").value;
+    var random = Math.floor(Math.random()*100 + 1);
 
 
-document.getElementById("calculateLove").addEventListener("click", ()=>{
-  var yourName = document.getElementById("yourName").value;
-  var theirName = document.getElementById("theirName").value;
-  var random = Math.floor(Math.random()*100 + 1);
-  if (random <= 40) {
-    document.getElementById("result").innerHTML = `Lovescore between ${yourName} and ${theirName} is ${random}%. It won't work.`;
-    document.getElementById("remarks").classList.add("bad");
-    document.getElementById("remarksss").classList.remove("perfect");
-    document.getElementById("remarkss").classList.remove("pairing");
-  } else if (random <=75) {
-    document.getElementById("result").innerHTML = `Lovescore between ${yourName} and ${theirName} is ${random}%. Congratulations, you're pairing.`;
-    document.getElementById("remarkss").classList.add("pairing");
-    document.getElementById("remarksss").classList.remove("perfect");
-    document.getElementById("remarks").classList.remove("bad");
-  } else {
-    document.getElementById("result").innerHTML = `Lovescore between ${yourName} and ${theirName} is ${random}%. Wow! You are made for each other.`;
-    document.getElementById("remarksss").classList.add("perfect");
-    document.getElementById("remarks").classList.remove("bad");
-    document.getElementById("remarkss").classList.remove("pairing");
-  }
-});
+var numberOfTimes = 3;
+
+    for (var i = 0; i < numberOfTimes; i++) {
+        if (random <= 40) {
+         document.getElementById("result").innerHTML = `Lovescore between ${yourName} and ${theirName} is ${random}%. It won't work.`;
+         document.getElementById("remarks").classList.add("bad");
+         document.getElementById("remarksss").classList.remove("perfect");
+         document.getElementById("remarkss").classList.remove("pairing");
+
+       } else if (random <=75) {
+         document.getElementById("result").innerHTML = `Lovescore between ${yourName} and ${theirName} is ${random}%. Congratulations, you're pairing.`;
+         document.getElementById("remarkss").classList.add("pairing");
+         document.getElementById("remarksss").classList.remove("perfect");
+         document.getElementById("remarks").classList.remove("bad");
+       } else {
+         document.getElementById("result").innerHTML = `Lovescore between ${yourName} and ${theirName} is ${random}%. Wow! You are made for each other.`;
+         document.getElementById("remarksss").classList.add("perfect");
+         document.getElementById("remarks").classList.remove("bad");
+         document.getElementById("remarkss").classList.remove("pairing");
+       }
+
+       while (random >= i) {
+           calc.style.display = "none";
+           break;
+       }
+       break;
+    }
+  });
+
+
